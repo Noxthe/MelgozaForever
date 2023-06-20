e00a240a974c9550fddd4b140792b1f87b7f4af4
Noxthe <83797031+Noxthe@users.noreply.github.com>
2023-06-19 22:17:52 -0600

    Add project files.

---
+++ MelgozaForever/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace MelgozaForever.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        
+        
+
+        public void OnGet()
+        {
+
+        }
+
+    }
+
+    public class Credential
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ MelgozaForever/Pages/MenuProductos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MelgozaForever.Pages
+{
+    public class MenuProductosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
