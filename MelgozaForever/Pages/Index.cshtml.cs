c98ab18044f0db1d647c33d523a42c8f7ee5e917
Noxthe <83797031+Noxthe@users.noreply.github.com>
2023-06-19 23:18:54 -0600

    no sé qué estoy haciendo jajaja

--- MelgozaForever/Pages/Index.cshtml.cs
+++ MelgozaForever/Pages/Index.cshtml.cs
@@ -7,29 +7,56 @@ namespace MelgozaForever.Pages
     public class IndexModel : PageModel
     {
         [BindProperty]
-        public Credential Credential { get; set; }
+        public Credential Credential { get; set; } = new Credential();
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
-        
-        
+
+
 
         public void OnGet()
         {
 
         }
+        private void SetUpLabels()
+        {
+
+        }
+        private bool CheckFields()
+        {
+            bool areValid = true;
+            if (Credential.Username == null || Credential.Username == "")
+            {
+                areValid = false;
+                ModelState.AddModelError("Credential.Username", "Username is required");
+            }
+            else if (Credential.Password == null || Credential.Password == "")
+            {
+                areValid = false;
+                ModelState.AddModelError("Credential.Password", "Password is required");
+            }
+            return areValid;
+        }
+        
+        private void Login()
+        {
+            if (CheckFields()) 
+            {
+
+            }
+        }
 
     }
-
+   
     public class Credential
     {
         [Required]
-        public string Username { get; set; }
+        public string? Username { get; set; }
         [Required]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string? Password { get; set; }
     }
 }
